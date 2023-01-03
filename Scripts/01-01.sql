364862d65eb80be7dc3eaf8aebf4311fff55842b
Jess Pomfret <jpomfret7@gmail.com>
2023-01-03 12:34:05 +0000

    format for screen

--- Scripts/01-01.sql
+++ Scripts/01-01.sql
@@ -1,6 +1,10 @@
 -- Create a list of employees and their immediate managers.
 
-SELECT emp.firstName, emp.lastName, emp.title, mng.firstName AS ManagerFirstName, mng.lastName AS ManagerLastName
+SELECT emp.firstName,
+    emp.lastName,
+    emp.title,
+    mng.firstName AS ManagerFirstName,
+    mng.lastName AS ManagerLastName
 FROM employee emp
 INNER JOIN employee mng
     ON emp.managerId = mng.employeeId
