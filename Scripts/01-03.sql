29a40a8c1b0b59963fb42016bc49c8a392edf2ab
Jess Pomfret <jpomfret7@gmail.com>
2023-01-06 17:30:51 +0000

    remove notes

--- Scripts/01-03.sql
+++ Scripts/01-03.sql
@@ -20,11 +20,3 @@ FROM sales sls
 LEFT JOIN customer cus
     ON cus.customerId = sls.customerId
 WHERE sls.salesId IS NULL;
-
--- Both UNION and UNION ALL operators combine
-rows from result sets into a single result set.
-The UNION operator removes eliminate duplicate rows,
-whereas the UNION ALL operator does not.
-
--- Because the UNION ALL operator does not remove duplicate rows,
---it runs faster than the UNION operator.
