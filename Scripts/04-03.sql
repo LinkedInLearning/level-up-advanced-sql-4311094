8504ecdd6d20726fad58a8fd2f4729f14f9df6c8
Jess Pomfret <jpomfret7@gmail.com>
2023-01-07 10:57:32 +0000

    break query down

--- Scripts/04-03.sql
+++ Scripts/04-03.sql
@@ -1,15 +1,22 @@
 -- Displays the number of cars sold this month, and last month
 
+-- Get the data
 SELECT strftime('%Y-%m', soldDate) AS MonthSold,
-  count(*) AS NumberCarsSold,
-  LAG (count(*), 1, 0 ) OVER win AS LastMonthCarsSold
+  count(*) AS NumberCarsSold
 FROM sales
 GROUP BY strftime('%Y-%m', soldDate)
-WINDOW win AS (ORDER BY strftime('%Y-%m', soldDate))
+
+-- Apply the window function
+SELECT strftime('%Y-%m', soldDate) AS MonthSold,
+  count(*) AS NumberCarsSold,
+  LAG (count(*), 1, 0 ) OVER calMonth AS LastMonthCarsSold
+FROM sales
+GROUP BY strftime('%Y-%m', soldDate)
+WINDOW calMonth AS (ORDER BY strftime('%Y-%m', soldDate))
 ORDER BY strftime('%Y-%m', soldDate)
 
 -- LAG to get last months
--- this time defining the window seperatly and then refering to that window in the select stateement
+-- this time defining the window seperatly and then refering to that window in the select statement
   -- this allows more flexibility as you to use the same window multiple times
   -- or you can use window chaining where one window can build on top of another
 
