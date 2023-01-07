8be215a7ae7e06922c61262cd780426e3d4fb186
Jess Pomfret <jpomfret7@gmail.com>
2023-01-07 11:00:10 +0000

    formatting

--- Scripts/03-01.sql
+++ Scripts/03-01.sql
@@ -1,10 +1,12 @@
 -- Summarise sales per year by using a CTE
 
 WITH cte AS (
-SELECT strftime('%Y', soldDate) AS soldYear, salesAmount
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  salesAmount
 FROM sales
 )
-SELECT soldYear, FORMAT("$%.2f", sum(salesAmount)) AS AnnualSales
+SELECT soldYear, 
+  FORMAT("$%.2f", sum(salesAmount)) AS AnnualSales
 FROM cte
 GROUP BY soldYear
 ORDER BY soldYear
