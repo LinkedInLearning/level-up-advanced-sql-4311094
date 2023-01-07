fc4794c9d0e478a3d842ecf98245da11416de723
Jess Pomfret <jpomfret7@gmail.com>
2023-01-07 10:36:43 +0000

    break queries up into chunks

--- Scripts/04-01.sql
+++ Scripts/04-01.sql
@@ -1,9 +1,18 @@
 -- For each sales person rank the car models they've sold most
 
+-- First join the tables to get the necessary data
+SELECT emp.firstName, emp.lastName, mdl.model, sls.salesId
+FROM sales sls
+INNER JOIN employee emp
+  ON sls.employeeId = emp.employeeId
+INNER JOIN inventory inv
+  ON inv.inventoryId = sls.inventoryId
+INNER JOIN model mdl
+  ON mdl.modelId = inv.modelId
 
+-- apply the grouping
 SELECT emp.firstName, emp.lastName, mdl.model,
-  count(model) AS NumberSold,
-  rank() OVER (PARTITION BY sls.employeeId ORDER BY count(model) desc) AS Rank
+  count(model) AS NumberSold
 FROM sales sls
 INNER JOIN employee emp
   ON sls.employeeId = emp.employeeId
@@ -13,24 +22,16 @@ INNER JOIN model mdl
   ON mdl.modelId = inv.modelId
 GROUP BY emp.firstName, emp.lastName, mdl.model
 
-
-/*
-
-https://www.sqlite.org/windowfunctions.html
-Window functions are distinguished from other SQL functions by the presence of an OVER clause. If a function has an OVER clause, then it is a window function. If it lacks an OVER clause, then it is an ordinary aggregate or scalar function. Window functions might also have a FILTER clause in between the function and the OVER clause.
-
-A window frame is used to specify how many rows around the current row the window should include.
-
-The RANK() function is a window function that assigns a rank to each row in a query’s result set. The rank of a row is calculated by one plus the number of ranks that comes before it.
-
-In this syntax:
-
-First, the PARTITION BY clause divides the rows of the result set into partitions.
-Second, the ORDER BY clause specifies the orders of the rows in each a partition.
-Third, the RANK() function is applied to each row in each partition and re-initialized when crossing the partition boundary.
-
-The same column values will receive the same ranks.
-When multiple rows have the same rank, the rank of the next row is not consecutive
-
-- could use DENSE_RANK if you didn't want gaps in rank
-*/
+-- add in the windowing function
+SELECT emp.firstName, emp.lastName, mdl.model,
+  count(model) AS NumberSold,
+  rank() OVER (PARTITION BY sls.employeeId 
+              ORDER BY count(model) desc) AS Rank
+FROM sales sls
+INNER JOIN employee emp
+  ON sls.employeeId = emp.employeeId
+INNER JOIN inventory inv
+  ON inv.inventoryId = sls.inventoryId
+INNER JOIN model mdl
+  ON mdl.modelId = inv.modelId
+GROUP BY emp.firstName, emp.lastName, mdl.model
--- Scripts/04-02.sql
+++ Scripts/04-02.sql
@@ -1,13 +1,33 @@
 -- Create a report showing sales per month and an annual total
 
-SELECT strftime('%Y', soldDate) AS soldYear, strftime('%m', soldDate) AS soldMonth, salesAmount,
-SUM (salesAmount) OVER (
-  PARTITION BY strftime('%Y', soldDate)
-  ORDER BY strftime('%m', soldDate)
-) AS AnnualRunningTotal
+-- get the needed data
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  strftime('%m', soldDate) AS soldMonth, 
+  salesAmount
+FROM sales
+
+-- apply the grouping
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  strftime('%m', soldDate) AS soldMonth,
+  SUM(salesAmount) AS salesAmount
 FROM sales
 GROUP BY soldYear, soldMonth
 ORDER BY  soldYear, soldMonth
+
+-- add the window function - simplify with cte
+with cte_sales as (
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  strftime('%m', soldDate) AS soldMonth,
+  SUM(salesAmount) AS salesAmount
+FROM sales
+GROUP BY soldYear, soldMonth
+)
+SELECT soldYear, soldMonth, salesAmount,
+  SUM(salesAmount) OVER (
+    PARTITION BY soldYear 
+    ORDER BY soldYear, soldMonth) AS AnnualSales_RunningTotal
+FROM cte_sales
+ORDER BY soldYear, soldMonth
 
 -- partition over the year so that the running total resets annually
 -- group by soldYear, soldMonth to collect the data from the sales table how we need it for the report
