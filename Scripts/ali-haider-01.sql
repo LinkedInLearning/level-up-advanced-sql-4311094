a0f46acc1e1778e3911c1e1faef12fa2f92ad2d4
Ali Haider <ali.enigma392@gmail.com>
2023-10-17 15:50:43 +0000

    first part done

--- Scripts/ali-haider-01.sql
+++ Scripts/ali-haider-01.sql
@@ -1,1 +1,12 @@
-SELECT * FROM employee LIMIT 10;
+select sql FROM sqlite_schema where name = "employee";
+
+select * from model m order by m.modelId desc LIMIT 10;
+
+SELECT * FROM employee e LIMIT 10;
+SELECT count(e.employeeId) FROM employee e;
+
+SELECT * FROM employee e LIMIT 10;
+SELECT * FROM sales s LIMIT 10;
+SELECT * FROM customer c LIMIT 10;
+SELECT * FROM model m LIMIT 10;
+SELECT * FROM inventory i LIMIT 10;
