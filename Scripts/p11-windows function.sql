6049e76559efa979aa690c59247dd0eefade1cc1
Ali Haider <ali.enigma392@gmail.com>
2023-10-22 16:08:26 +0000

    course completed successfully

--- Scripts/p11-windows function.sql
+++ Scripts/p11-windows function.sql
@@ -1,1 +1,6 @@
-select strftime('%Y', s.soldDate) from sales s;
+select strftime('%Y-%m', s.soldDate) as soldMonth,
+  COUNT(s.salesId) as carsSold,
+  LAG (count(s.salesId), 1, 0) OVER calMonth as carsSoldLastMonth
+  from sales s
+  GROUP BY soldMonth
+  WINDOW calMonth AS (ORDER BY strftime('%Y-%m', s.soldDate));
