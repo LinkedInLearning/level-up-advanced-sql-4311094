160259adc97cbb9df69f6ee1dc4fe5ead5116e03
Snowstache <149518726+Snowstache@users.noreply.github.com>
2024-01-16 19:24:41 +0000

    Added Exerc 6

--- Scripts/AdvancedSQL1.sql
+++ Scripts/AdvancedSQL1.sql
@@ -82,3 +82,16 @@ WHERE sls.soldDate >= date('now', 'start of year')
 GROUP BY emp.employeeId, emp.firstName, emp.lastName
 
 
+-- 6. ----------
+-- Display report for employees who have sold AT LEAST 5 cars
+
+SELECT emp.employeeId,
+    count(*) AS NumOfCarsSold
+    MIN(salesAmount) AS MinSalesAmount
+    MAX(salesAmount) AS MaxSalesAmount
+FROM sales sls 
+INNER JOIN employee emp 
+    ON sls.employeeId = emp.employeeId
+WHERE sls.soldDate >= date('now', 'start of year')
+GROUP BY emp.employeeId
+HAVING count(*) > 5
